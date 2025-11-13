ec9f51d4bb30bb0390397f8485f875335549b733
sylvornyx <morkaths@gmail.com>
2025-11-13 21:53:38 +0700

    feat: Add seed and start scripts for IDoc API services

---
+++ seed.bat
@@ -0,0 +1,11 @@
+@REM Seed Script for IDoc API Services
+@REM CMD: seed.bat
+@REM PowerShell: .\seed.bat
+
+@echo off
+
+@REM start cmd /k "cd /d %~dp0auth-service && mvn spring-boot:run -Dspring-boot.run.profiles=mock"
+
+start cmd /k "cd /d %~dp0user-service && npm run seed"
+
+start cmd /k "cd /d %~dp0book-service && npm run seed"
---
+++ start.bat
@@ -0,0 +1,7 @@
+@REM CMD: start.bat 
+@REM PowerShell: .\start.bat
+@echo off
+
+start cmd /k "cd /d %~dp0auth-service && mvn spring-boot:run"
+start cmd /k "cd /d %~dp0user-service && npm run dev"
+start cmd /k "cd /d %~dp0book-service && npm run dev"
